3691f204353da997ed4940e2f82d70f82165173d
Jorge Leon <jorgeleonpicture@gmail.com>
2022-04-23 17:05:26 +0000

    finished exercises

--- .learn/resets/10.1-Creating-Your-First-Function/app.py
+++ .learn/resets/10.1-Creating-Your-First-Function/app.py
@@ -1,6 +1,5 @@
 def addNumbers(a,b):
 	# This is the function body. Write your code here.
-	return int(a + b)
   
 
 # Do not change the code below
---
+++ .learn/resets/18-The-Beatles/app.py
@@ -0,0 +1,14 @@
+# Your code here!!
+
+
+song = []
+letter = 'let it be'
+for i in range(0,13):
+    i = 'let it be'
+    song.append(i)
+    if song == 4:
+        song
+    
+    print (song)
+
+
